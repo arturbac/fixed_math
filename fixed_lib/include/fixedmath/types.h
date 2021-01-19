66248a985bf2d75acfad27590f15986303f91838
Artur Bac (linux) <ghaqurat@ebasoft.com.pl>
2021-01-19 23:08:17 +0100

    remove junk

--- fixed_lib/include/fixedmath/types.h
+++ fixed_lib/include/fixedmath/types.h
@@ -32,8 +32,8 @@
 
 #define FIXEDMATH_VERSION_MAJOR 0
 #define FIXEDMATH_VERSION_MINOR 9
-#define FIXEDMATH_VERSION_REVISION 0
-#define FIXEDMATH_VERSION_STRING "fixedmath 0.9.0"
+#define FIXEDMATH_VERSION_PATCH 0
+#define FIXEDMATH_VERSION_STRING "fixedmath 0.9.0 beta"
 
 namespace fixedmath
 {
